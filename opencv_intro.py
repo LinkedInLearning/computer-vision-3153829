44d02d1950bf0d9909cac63e316bd301a6c97ec9
Matthew Scarpino <mattscar@gmail.com>
2021-08-12 18:04:58 -0400

    Beginning files of 02_03

---
+++ opencv_intro.py
@@ -0,0 +1,15 @@
+# Read the image
+
+# Remove the blue content
+
+# Make the first ten rows gray
+
+# Center the image by subtracting the mean
+
+# Standarize the image by dividing the standard deviation
+
+# Prepare the new image to be saved
+
+# Print the mean and stddev on the image
+
+# Save the output image
