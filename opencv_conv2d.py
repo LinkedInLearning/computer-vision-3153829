f4b13d6529c52af86fbace59541bf70a85889327
Matthew Scarpino <mattscar@gmail.com>
2021-08-12 18:10:47 -0400

    Beginning files for 02_05

---
+++ opencv_conv2d.py
@@ -0,0 +1,13 @@
+# Read the image
+
+# Convolve with the box blur kernel
+
+# Display convolution
+
+# Convolve with the gaussian kernel
+
+# Display convolution
+
+# Convolve with the sharpen kernel
+
+# Display convolution
