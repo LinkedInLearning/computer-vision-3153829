ce9a09c98476b87aa0982c6d5d4589065a142370
Matthew Scarpino <mattscar@gmail.com>
2021-08-20 10:15:25 -0400

    Files for 05_04

---
+++ object_recognition.py
@@ -0,0 +1,13 @@
+# Create interpreter
+
+# Get input and output indices
+
+# Load test images
+    
+    # Get image and label
+    
+    # Send image to interpreter
+    
+    # Launch interpreter and get prediction
+
+# Display accuracy
