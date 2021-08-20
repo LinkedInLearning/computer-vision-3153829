94bbc58810c3cb9df269f1d94214131b163fe79b
Matthew Scarpino <mattscar@gmail.com>
2021-08-19 21:43:04 -0400

    Final file for 03_05

--- object_detection.py
+++ object_detection.py
@@ -20,15 +20,40 @@ for img_file in listdir(img_dir):
     img /= np.std(img)
     img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
     train_features.append(hog.compute(img))
-    print(train_features[0].shape)
-    break
-    
+    train_labels.append(int(img_file.split('_')[0]))
+
 # Create linear SVM
+svm = cv2.ml.SVM_create()
+svm.setKernel(cv2.ml.SVM_LINEAR)
 
 # Train and save SVM
+svm.train(np.array(train_features), cv2.ml.ROW_SAMPLE, np.array(train_labels))
+svm.save("svm.xml")
 
 # Configure detector
+vec = svm.getSupportVectors()
+rho, _, _ = svm.getDecisionFunction(0)
+vec = np.append(vec, -rho)
+hog.setSVMDetector(vec)
 
 # Load test images
+test_images = []
+img_dir = "test_imgs"
+for img_file in listdir(img_dir):
+    img = cv2.imread(img_dir + "/" + img_file, cv2.IMREAD_COLOR)
+    img = img.astype(np.float64) - np.mean(img)
+    img /= np.std(img)
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    test_images.append(img)
 
 # Test detector
+for test_image in test_images:
+    res = hog.detectMultiScale(test_image)
+    if len(res[1]) > 0 and np.max(res[1]) > 1.2:
+        index = np.argmax(res[1])
+        (x, y, w, h) = res[0][index]
+        cv2.rectangle(test_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('result', test_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
