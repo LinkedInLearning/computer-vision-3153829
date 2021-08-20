def809001db1663037dd52195b1fddde2ba78fc7
Matthew Scarpino <mattscar@gmail.com>
2021-08-20 09:25:19 -0400

    Beginning files for 04_03

---
+++ iris_classifier.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import csv
+
+# Create lists for training/test data
+
+# Load data from CSV file
+
+# Create neural network
+
+# Train neural network
+
+# Classify test points
+
+# Print error
