50ca08e75d44ee2d591e8363997f436d2edc1110
Matthew Scarpino <mattscar@gmail.com>
2021-08-21 09:48:09 -0400

    Final file for 06_01

--- camera_intro.py
+++ camera_intro.py
@@ -1,3 +1,14 @@
+import picamera
+import picamera.array
+import cv2
+
 # Access camera and stream
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
    
         # Capture images
+        for i in range(10):
+            camera.capture(stream, format='bgr')
+            img = stream.array
+            cv2.imwrite('img_{}.jpg'.format(i), img)
+            stream.seek(0)
