96a399335a917a2d1db1ad928f75a518a59da76b
Matthew Scarpino <mattscar@gmail.com>
2021-08-25 11:02:54 -0400

    Beginning file for 06_02

---
+++ object_detection_camera.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import picamera
+import picamera.array
+import time
+
+# Create HOG descriptor 
+
+# Load SVM and configure descriptor
+
+# Access camera and stream
+    
+# Preprocess and analyze images
+            
+    # Perform object detection and save result
