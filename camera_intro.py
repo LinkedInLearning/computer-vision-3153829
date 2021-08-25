ee9f52366b78ce5dad08c8ea10ad041ef44beacd
Matthew Scarpino <mattscar@gmail.com>
2021-08-25 10:31:06 -0400

    Updated camera intro

--- camera_intro.py
+++ camera_intro.py
@@ -1,14 +1,20 @@
 import picamera
 import picamera.array
 import cv2
+import time
 
 # Access camera and stream
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as stream:
-   
-        # Capture images
-        for i in range(10):
-            camera.capture(stream, format='bgr')
-            img = stream.array
-            cv2.imwrite('img_{}.jpg'.format(i), img)
-            stream.seek(0)
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.brightness = 60
+camera.contrast = 20
+camera.iso = 1600
+stream = picamera.array.PiRGBArray(camera)
+        
+# Capture images
+for i in range(10):
+    camera.capture(stream, format='bgr')
+    img = stream.array
+    cv2.imwrite('img_{}.jpg'.format(i), img)
+    stream.seek(0)
+    time.sleep(0.5)
