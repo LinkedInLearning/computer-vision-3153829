3f19b7484be30170067d46b5a5b214b4aef76804
Matthew Scarpino <mattscar@gmail.com>
2021-08-26 12:43:45 -0400

    Initial file for 06_03

---
+++ object_recognition_camera_b.py
@@ -0,0 +1,27 @@
+import tflite_runtime.interpreter as tflite
+import numpy as np
+import picamera
+import picamera.array
+import time
+import io
+from PIL import Image, ImageDraw
+
+# Create interpreter
+
+
+# Get input and output indices
+
+
+# Access camera and stream
+
+
+# Preprocess and analyze images
+
+
+    # Convert to NumPy array
+
+
+    # Send image to interpreter
+
+
+    # Draw on image and save to file
