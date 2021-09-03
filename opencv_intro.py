3be09a8fa2bd26af90b7b42c337fbfde33b28248
Matthew Scarpino <mattscar@gmail.com>
2021-09-03 15:37:49 -0400

    Beginning file for 02_03

--- opencv_intro.py
+++ opencv_intro.py
@@ -1,15 +1,28 @@
+import cv2
+import numpy as np
+
 # Read the image
 
-# Remove the blue content
+
+# Remove blue content
+
 
 # Make the first ten rows gray
 
+
 # Center the image by subtracting the mean
+
 
 # Standarize the image by dividing the standard deviation
 
+
+# Compute statistics
+
+
 # Prepare the new image to be saved
 
+
 # Print the mean and stddev on the image
+
 
 # Save the output image
