011b67759df0bbe731d9497dd6d009030e735d39
Matthew Scarpino <mattscar@gmail.com>
2021-09-02 22:16:09 -0400

    Final file for 01_03

--- thonny_intro.py
+++ thonny_intro.py
@@ -1,10 +1,13 @@
 # Import packages
-
+import cv2
+import numpy as np
 
 # Create array
-
+img_array = np.full((400, 600), 0.5)
 
 # Display array as image
-
+cv2.imshow("Window", img_array)
 
 # Wait for a keypress
+cv2.waitKey()
+cv2.destroyAllWindows()
