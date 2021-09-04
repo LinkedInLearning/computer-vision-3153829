d47aa1524aba697791fcfdc62f864f44227d4b27
Matthew Scarpino <mattscar@gmail.com>
2021-09-04 10:58:37 -0400

    Starting file for 02_05

--- opencv_conv2d.py
+++ opencv_conv2d.py
@@ -1,13 +1,20 @@
+import cv2
+import numpy as np
+
+# Create image window
+def display(name, image):
+    cv2.imshow(name, image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
 # Read the image
+
 
 # Convolve with the box blur kernel
 
-# Display convolution
 
 # Convolve with the gaussian kernel
 
-# Display convolution
 
 # Convolve with the sharpen kernel
 
-# Display convolution
