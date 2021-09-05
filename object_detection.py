ed7a3d278adeced39ae832cd914105b1db9065c1
Matthew Scarpino <mattscar@gmail.com>
2021-09-05 11:41:57 -0400

    Final file for 03_03

--- object_detection.py
+++ object_detection.py
@@ -2,20 +2,34 @@ import cv2
 import numpy as np
 from os import listdir
 
-# Configure HOG detector 
-win_size = (32, 96)
-block_size = (16, 16)
-block_stride = (16, 16)
+# Create image window
+def display(name, image):
+    cv2.imshow(name, image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+# Preprocess images
+def preprocess(img_dir, img_file):
+    img = cv2.imread(img_dir + '/' + img_file, cv2.IMREAD_COLOR)    
+    img = img.astype(np.float64) - np.mean(img)
+    img /= np.std(img)
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    return img
+
+# Create HOG detector
+window_size = (32, 96)
 cell_size = (8, 8)
 nbins = 9
-hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+block_size = (32, 32)
+block_stride = (32, 32)
+hog = cv2.HOGDescriptor(window_size, block_size, block_stride, cell_size, nbins)
 
 # Read training images
 train_features = []
 train_labels = []
-img_dir = "train_imgs"
+img_dir = 'train_imgs'
 for img_file in listdir(img_dir):
-    img = cv2.imread(img_dir + "/" + img_file, cv2.IMREAD_COLOR)
+    img = cv2.imread(img_dir + '/' + img_file, cv2.IMREAD_COLOR)
     img = img.astype(np.float64) - np.mean(img)
     img /= np.std(img)
     img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
