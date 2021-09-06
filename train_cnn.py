14594710ad46fc932c642fafe14e4f79e9ee3830
Matthew Scarpino <mattscar@gmail.com>
2021-09-05 22:27:25 -0400

    Ending file for 05_03

--- train_cnn.py
+++ train_cnn.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -5,20 +8,27 @@ from tensorflow.keras import layers, models
 from keras.preprocessing import image
 from os import listdir
 
-# Load training data
-imgs = []
-labels = []
-num_classes = 4
-img_dir = 'train_imgs'
-for img_file in listdir(img_dir):
-    img = image.img_to_array(image.load_img(img_dir + '/' + img_file))
+# Preprocess images
+def preprocess(img_dir, img_file):
+    img = image.img_to_array(image.load_img(img_dir + '/' + img_file))    
     img = img.astype(np.float64) - np.mean(img)
     img /= np.std(img)
-    imgs.append(img)
-    label = int(img_file.split('_')[0])
-    labels.append(np.eye(num_classes)[label])
+    return img
 
-# Create model
+# Load training data
+def load_data(img_dir, num_classes):
+    for img_file in listdir(img_dir):
+        imgs = []
+        labels = []
+        img = preprocess(img_dir, img_file)
+        imgs.append(img)
+        label = int(img_file.split('_')[0])
+        labels.append(np.eye(num_classes)[label])
+        return imgs, labels
+
+# Initialize the model
+num_classes = 4
+imgs, labels = load_data('train_imgs', num_classes)
 model = models.Sequential()
 model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=imgs[0].shape))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -27,12 +37,12 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
 model.add(layers.Dense(num_classes, activation='softmax'))
 
-# Set training parameters
+# Compile the model
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
-model.fit(np.asarray(imgs), np.asarray(labels), epochs=10)
+model.fit(np.asarray(imgs), np.asarray(labels), epochs=10, verbose=0)
 
-# Save the model
+# Save the model in TensorFlow Lite format
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 open('object_recognition.tflite', 'wb').write(converter.convert())
