5f2970c44cbdb1ad94fa9a9dc5a9b87c0cdacd87
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:18:53 -0400

    Starting file for 06_04

--- object_recognition_camera.py
+++ object_recognition_camera.py
@@ -1,10 +1,27 @@
 import tflite_runtime.interpreter as tflite
 import numpy as np
 import picamera
-import picamera.array
 import time
 import io
 from PIL import Image, ImageDraw
+
+# Preprocess images
+def preprocess(image):
+    image = np.array(image)
+    image = image.astype(np.float32) - np.mean(image)
+    image /= np.std(image)
+    image = np.expand_dims(image, axis=0)
+    return image
+
+# Access camera and stream
+def get_camera_stream():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.brightness = 60
+    camera.contrast = 20
+    camera.iso = 1600
+    stream = io.BytesIO()
+    return camera, stream
 
 # Create interpreter
 
@@ -12,16 +29,12 @@ from PIL import Image, ImageDraw
 # Get input and output indices
 
 
-# Access camera and stream
-
-
 # Preprocess and analyze images
-
-
-    # Convert to NumPy array
 
 
     # Send image to interpreter
 
 
     # Draw on image and save to file
+
+
