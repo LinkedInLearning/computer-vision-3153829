253bd48a98d517eb93c3dd0f27b29f80ce3023a8
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:16:40 -0400

    Final file for 06_03

--- object_detection_camera.py
+++ object_detection_camera.py
@@ -4,36 +4,47 @@ import picamera
 import picamera.array
 import time
 
-# Create HOG descriptor
-win_size = (32, 96)
-cell_size = (8, 8)
-nbins = 9
-block_size = (16, 16)
-block_stride = (16, 16)
-hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+# Preprocess image
+def preprocess(img, size):
+    img = cv2.resize(img, size)
+    img = img.astype(np.float64) - np.mean(img)
+    img /= np.std(img)
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    return img
 
-# Load SVM and configure descriptor
+# Create HOG descriptor
+def get_descriptor():
+    win_size = (32, 96)
+    cell_size = (8, 8)
+    nbins = 9
+    block_size = (16, 16)
+    block_stride = (16, 16)
+    hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+    return hog
+
+# Access camera and stream
+def get_camera_stream():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.brightness = 60
+    camera.contrast = 20
+    camera.iso = 1600
+    stream = picamera.array.PiRGBArray(camera)
+    return camera, stream
+
+# Configure HOG descriptor with SVM
+hog = get_descriptor()
 svm = cv2.ml.SVM_load('svm.xml')
 vec = svm.getSupportVectors()
 rho, _, _ = svm.getDecisionFunction(0)
 vec = np.append(vec, -rho)
 hog.setSVMDetector(vec)
 
-# Access camera and stream
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.brightness = 60
-camera.contrast = 20
-camera.iso = 1600
-stream = picamera.array.PiRGBArray(camera)
-
 # Preprocess and analyze images
+camera, stream = get_camera_stream()
 for i in range(10):
     camera.capture(stream, format='bgr')
-    test_image = cv2.resize(stream.array, (213, 160))
-    test_image = test_image.astype(np.float64) - np.mean(test_image)
-    test_image /= np.std(test_image)
-    test_image = cv2.normalize(test_image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    test_image = preprocess(stream.array, (213, 160))
 
     # Perform object detection and save result
     res = hog.detectMultiScale(test_image)
@@ -44,3 +55,4 @@ for i in range(10):
     cv2.imwrite('img_{}.jpg'.format(i), test_image)
     stream.seek(0)
     time.sleep(0.5)
+
