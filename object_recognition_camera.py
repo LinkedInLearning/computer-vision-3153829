d2587d771f6264e4c077bd37f5da6eb807ab491c
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:21:12 -0400

    Ending file for 06_04

--- object_recognition_camera.py
+++ object_recognition_camera.py
@@ -1,49 +1,54 @@
 import tflite_runtime.interpreter as tflite
 import numpy as np
 import picamera
-import picamera.array
 import time
 import io
 from PIL import Image, ImageDraw
 
-# Create interpreter
-interpreter = tflite.Interpreter(model_path='object_recognition.tflite')
-interpreter.allocate_tensors()
-
-# Get input and output indices
-input_index = interpreter.get_input_details()[0]['index']
-output_index = interpreter.get_output_details()[0]['index']
+# Preprocess images
+def preprocess(image):
+    image = np.array(image)
+    image = image.astype(np.float32) - np.mean(image)
+    image /= np.std(image)
+    image = np.expand_dims(image, axis=0)
+    return image
 
 # Access camera and stream
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.brightness = 60
-camera.contrast = 20
-camera.iso = 1600
-stream = io.BytesIO()
+def get_camera_stream():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.brightness = 60
+    camera.contrast = 20
+    camera.iso = 1600
+    stream = io.BytesIO()
+    return camera, stream
+
+# Create interpreter
+ip = tflite.Interpreter(model_path='object_recognition.tflite')
+ip.allocate_tensors()
+
+# Get input and output indices
+input_index = ip.get_input_details()[0]['index']
+output_index = ip.get_output_details()[0]['index']
 
 # Preprocess and analyze images
+camera, stream = get_camera_stream()
 for i in range(10):
     camera.capture(stream, format='jpeg')
     stream.seek(0)
-    pil_image = Image.open(stream).crop((80, 0, 560, 480))
-    pil_image = pil_image.resize((256, 256))
-
-    # Convert to NumPy array
-    test_image = np.array(pil_image)
-    test_image = test_image.astype(np.float32) - np.mean(test_image)
-    test_image /= np.std(test_image)
+    img = Image.open(stream).crop((80, 0, 560, 480))
+    img = img.resize((256, 256))
+    tensor = preprocess(img)
 
     # Send image to interpreter
-    test_image = np.expand_dims(test_image, axis=0)
-    interpreter.set_tensor(input_index, test_image)
-    interpreter.invoke()
-    pred = interpreter.get_tensor(output_index)
+    ip.set_tensor(input_index, tensor)
+    ip.invoke()
+    pred = ip.get_tensor(output_index)
 
     # Draw on image and save to file
-    draw = ImageDraw.Draw(pil_image)
-    res_str = 'pred: {}, prob: {}'.format(np.argmax(pred), np.max(pred))
-    draw.text((10, 10), res_str)
-    pil_image.save('img_{}.jpg'.format(i))
-    stream.seek(0)
+    draw = ImageDraw.Draw(img)
+    msg = 'pred: {}, prob: {}'.format(np.argmax(pred), np.max(pred))
+    draw.text((10, 10), msg)
+    img.save('img_{}.jpg'.format(i))
     time.sleep(0.5)
+
