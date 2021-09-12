856856796b4f750775747d9c0136a963977c43fa
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:46:31 -0400

    Starting file for 04_04

--- iris_classifier.py
+++ iris_classifier.py
@@ -4,12 +4,17 @@ import csv
 
 # Create lists for training/test data
 
+
 # Load data from CSV file
+
 
 # Create neural network
 
+
 # Train neural network
 
+
 # Classify test points
+
 
 # Print error
