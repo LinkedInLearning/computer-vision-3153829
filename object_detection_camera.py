2962898575de697f92fd2cf8cf7351a9f1ab788a
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:14:12 -0400

    Starting file for 06_03

--- object_detection_camera.py
+++ object_detection_camera.py
@@ -4,17 +4,40 @@ import picamera
 import picamera.array
 import time
 
-# Create HOG descriptor 
+# Preprocess image
+def preprocess(img, size):
+    img = cv2.resize(img, size)
+    img = img.astype(np.float64) - np.mean(img)
+    img /= np.std(img)
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    return img
 
-
-# Load SVM and configure descriptor
-
+# Create HOG descriptor
+def get_descriptor():
+    win_size = (32, 96)
+    cell_size = (8, 8)
+    nbins = 9
+    block_size = (16, 16)
+    block_stride = (16, 16)
+    hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+    return hog
 
 # Access camera and stream
+def get_camera_stream():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.brightness = 60
+    camera.contrast = 20
+    camera.iso = 1600
+    stream = picamera.array.PiRGBArray(camera)
+    return camera, stream
 
-    
+# Configure HOG descriptor with SVM
+
+
 # Preprocess and analyze images
 
-            
+
     # Perform object detection and save result
-    
+
+
