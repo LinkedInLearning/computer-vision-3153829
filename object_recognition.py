d9d4cf42f00d8888454597f4ce25c90f4c26df01
Matthew Scarpino <mattscar@gmail.com>
2021-09-12 11:26:29 -0400

    Starting file for 05_05

--- object_recognition.py
+++ object_recognition.py
@@ -1,13 +1,32 @@
+from PIL import Image
+import tflite_runtime.ip as tflite
+import numpy as np
+from os import listdir
+
+# Preprocess images
+def preprocess(img_dir, img_file):
+    img = np.array(Image.open(img_dir + '/' + img_file))
+    img = img.astype(np.float32) - np.mean(img)
+    img /= np.std(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
 # Create interpreter
 
-# Get input and output indices
+
+# Get input/output indices
+
 
 # Load test images
-    
-    # Get image and label
+
     
     # Send image to interpreter
-    
+
+
     # Launch interpreter and get prediction
+
+    
+    # Test classifications
+
 
 # Display accuracy
